bee4ede31f1713d7d5a7ff894fb35243f403a947
eliaschen <eliaschen.dev@gmail.com>
2024-10-23 10:50:36 +0800

    add some buttons with message box

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "訊息方塊";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("新細明體", 16F);
+            this.result.Location = new System.Drawing.Point(12, 338);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 22);
+            this.result.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "判斷按鈕";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(508, 369);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,23 @@ namespace SAIHS_CSHARP_2_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult callBack = MessageBox.Show("是否有兩個按鈕的訊息方塊","訊息方塊練習",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var callBack = MessageBox.Show("Am I cute?", "Simple Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(callBack == DialogResult.Yes)
+            {
+                MessageBox.Show("Yaaaaa!","Reply",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Fine :(","Reply",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
     }
 }
