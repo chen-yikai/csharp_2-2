70abd873e36ae8e3e6a9387bb5f6dfbee06b6045
eliaschen <eliaschen.dev@gmail.com>
2024-10-23 12:05:21 +0800

    1024/10/23 final commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,10 +31,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.result = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.res = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 15F);
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 58);
@@ -54,19 +61,86 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 15F);
             this.button2.Location = new System.Drawing.Point(166, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(128, 58);
             this.button2.TabIndex = 2;
-            this.button2.Text = "判斷按鈕";
+            this.button2.Text = "判斷按鈕1";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button3.Location = new System.Drawing.Point(12, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "判斷按鈕2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button4.Location = new System.Drawing.Point(166, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "判斷按鈕3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.textBox1.Location = new System.Drawing.Point(12, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("新細明體", 15F);
+            this.button5.Location = new System.Drawing.Point(143, 195);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "確認";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label1.Location = new System.Drawing.Point(8, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "判斷奇數偶數";
+            // 
+            // res
+            // 
+            this.res.AutoSize = true;
+            this.res.Font = new System.Drawing.Font("新細明體", 15F);
+            this.res.Location = new System.Drawing.Point(12, 238);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(0, 20);
+            this.res.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(508, 369);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.result);
             this.Controls.Add(this.button1);
@@ -82,6 +156,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label result;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label res;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -34,5 +34,45 @@ namespace SAIHS_CSHARP_2_2
                 MessageBox.Show("Fine :(","Reply",MessageBoxButtons.OK,MessageBoxIcon.Information);
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var callBack  = MessageBox.Show("看你按了哪個按鈕","按鈕判斷二",MessageBoxButtons.OKCancel,MessageBoxIcon.Question);
+            if(callBack == DialogResult.OK)
+            {
+                MessageBox.Show("你按了 確定","回復",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                MessageBox.Show("你按了 取消", "回復", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var callBack = MessageBox.Show("看你按了哪個按鈕", "按鈕判斷二", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Question);
+            if (callBack == DialogResult.Abort)
+            {
+                MessageBox.Show("你按了 中止", "回復", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                if (callBack == DialogResult.Retry) { 
+                    MessageBox.Show("你按了 重試", "回復", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }else { 
+                    MessageBox.Show("你按了 略過","回復",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int input = (int)Convert.ToInt32(textBox1.Text);
+            res.Text = input == 0 ? string.Empty : (input % 2 == 0 ? input + "是偶數" : input + "是奇數");
+           
+        }
     }
 }
